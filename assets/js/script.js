aeb9291e1c508e40fdf23d523e9f6ae2f59d9e12
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-17 16:01:52 -0400

    drag and drop completed, including the trash zone

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,21 +54,21 @@ $(".list-group").on("click", "p", function(){
 
     //get the textarea's current value/text
     var text=$(this)
-    .val()
-    .trim();
+    .val();
+    //.trim();
 
     //get the parent ul's id attribute
     var status = $(this)
     .closest(".list-group")
     .attr("id")
-    .replace ("list-", "");
+    .replace("list-", "");
 
     //get the task's position in the list of other li elements
     var index = $(this)
     .closest(".list-group-item")
     .index();
 
-    tasks[status][index].text =text;
+    tasks[status][index].text = text;
     saveTasks();
 
     //recreate p element
@@ -176,23 +176,6 @@ $(".list-group").on("blur", "input[type='text']", function(){
   $(this).replaceWith(taskSpan);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -200,6 +183,77 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+$(".card .list-group").sortable({
+  //connect the group with other groups with the same className
+  connectWith: $(".card .list-group"),
+  //so the page won't scroll when item is dragged.
+  scroll: false,
+  //which mode to use for testing when the item is hovering over the other item.
+  tolerance: "pointer",
+  //allows helper element to be used for dragging display, the element will be cloned and the clone will be dragged.
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    //array to store the task data in
+    var tempArr = [];
+    //loop over current set of children in sortable list
+    $(this).children().each(function(){
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      //add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    //trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    //update array on task object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    console.log(tempArr)
+  }
+
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui){
+    console.log("out");
+  }
 });
 
 // load tasks for the first time
